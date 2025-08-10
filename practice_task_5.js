f898f9ec99073b8e5f37104ae81d8087eeefaa57
IbrahimNewaz <ibrahim.codecoffe@gmail.com>
2025-08-11 02:42:09 +0600

    fixed the bug

--- practice_task_5.js
+++ practice_task_5.js
@@ -11,7 +11,8 @@ Original: [1, 2, 3] Copy: [99, 2, 3]
 
 const arr1 = [1, 2, 3];
 console.log('The Original Array -> ', arr1);
-const arr2 = arr1; // copy 1st arry to a new 2nd array //
+// const arr2 = arr 1 // <- this is not the right way to copy an array //
+const arr2 = [...arr1]; // copy 1st arry to a new 2nd array //
 console.log('Copy Array -> ', arr2);
 arr2[0] = 99;
 console.log('Change 1st Elements of Copyed Arrays 1st Index -> ', arr2);
